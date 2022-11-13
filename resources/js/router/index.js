3b0fbe673843df220b0cd54e0247d64af9ff6808
kita127 <kitada.kosuke@gmail.com>
2022-11-13 21:44:20 +0900

    タスク詳細コンポーネント実装

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TaskListComponent from '../components/TaskListComponent.vue'
+import TaskShowComponent from "../components/TaskShowComponent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
       path: '/tasks',
       name: 'task.list',
       component: TaskListComponent
+    },
+    {
+      path: '/tasks/:taskId',
+      name: 'task.show',
+      component: TaskShowComponent,
+      props: true
     },
   ]
 })
