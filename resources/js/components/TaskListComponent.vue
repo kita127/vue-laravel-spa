b97ba3aa8db37a551f59a7d41a90a154117a88a6
kita127 <kitada.kosuke@gmail.com>
2022-11-13 22:17:54 +0900

    一覧表示とAPIの連携実装

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -66,9 +66,46 @@
                 </tr>
             </tbody>
         </table>
+        <table class="table table-hover">
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Show Button</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(task, index) in tasks" :key="index">
+                    <th scope="row">{{ task.id }}</th>
+                    <td>{{ task.name }}</td>
+                    <td>
+                        <router-link v-bind:to="{ name: 'task.show', params: { taskId: task.id } }">
+                            <button class="btn btn-primary">Show</button>
+                        </router-link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data: function () {
+        return {
+            tasks: []
+        }
+    },
+    methods: {
+        getTasks() {
+            axios.get('/api/tasks')
+                .then((res) => {
+                    this.tasks = res.data;
+                });
+        }
+    },
+    mounted() {
+        this.getTasks();
+    }
+}
 </script>
