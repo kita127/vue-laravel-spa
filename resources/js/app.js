128a3b6d95bf36637bbb1185d7336ccb7abc2be5
kita127 <kitada.kosuke@gmail.com>
2022-11-13 22:23:54 +0900

    ./router/index.jsに変更わかりやすい

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@
 
 import './bootstrap';
 import { createApp } from 'vue';
-import router from './router';
+import router from './router/index.js';
 
 /**
  * Next, we will create a fresh Vue application instance. You may then begin
