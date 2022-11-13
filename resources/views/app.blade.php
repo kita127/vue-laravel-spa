1cae4d331cecb1475a0d1a48eae10ca40fe9410e
kita127 <kitada.kosuke@gmail.com>
2022-11-13 17:25:15 +0900

    bootstrap使用時のviteの指定が誤っていたため修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     <title>{{ config('app.name', 'Vue Laravel SPA') }}</title>
 
